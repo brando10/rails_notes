7aa0c6f28d28231c34eaa98788716756f3bc039c
Dark_Lourd <marthen7@gmail.com>
2017-09-27 14:05:45 +0800

    sidekiq notes

---
+++ sidekiq.rb
@@ -0,0 +1,11 @@
+#SEARCH SIDEKIQ INSTACE
+ps aux | grep sidekiq
+ 
+#KILL SIDEKIQ INSTANCE
+kill -9 #{sidekiq instance}
+
+#START SIDEKIQ IN PROODUCTION
+bundle exec sidekiq -d -L log/sidekiq.log -C config/sidekiq.yml -e production
+
+#START SIDEKIQ IN DEVELOPMENT
+bundle exec sidekiq
