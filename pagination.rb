4d8598c721965ee9d2896438198e0d19b71f8458
Dark_Lourd <marthen7@gmail.com>
2017-09-27 13:58:18 +0800

    Pagination Notes

--- pagination.rb
+++ pagination.rb
@@ -1,2 +1,13 @@
 WILL PAGINATION GEM
 
+gem 'will_paginate', '>= 3.1'
+
+Bundle install
+
+Restart Rails Server
+
+#SET TO CONTROLLER
+@trans = Transaction.paginate(page: params[:page], per_page: 10)
+
+#SET to VIEW
+= will_paginate @trans
