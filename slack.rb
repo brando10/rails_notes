4184e40fcd54ef0935e545f84998c3cf02a318e7
Martin <marthen7@gmail.com>
2019-03-18 16:51:35 +0800

    new note for deploying slate api documentation

---
+++ slack.rb
@@ -0,0 +1,15 @@
+Setting UP
+
+1. Clone from slate app and change path name
+2. Change Ruby version higher than 2.4 and above
+3. Bundle install
+4. Click button (Depiloy to Heroku) on github readme page
+5. Create new app in heroku and deploy.
+
+Update and Changes to app.
+
+1. Edit and update done on the codebase api
+2. Git push to repo
+3. Find the app which caters to the code from heroku (copy name and destroy instance)
+4. Click button (Depiloy to Heroku) on github readme page
+5. Create new app in heroku and deploy. (naming of app should be the same from last destroyed instance)
